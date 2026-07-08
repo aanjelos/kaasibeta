1540e6fd3bb9729ff6e72f01930d0bc6ab0f8b67
aanjelos <aanjelo99@gmail.com>
2026-07-08 12:25:14 +0530

    chore: clean up scratch a11y scripts

--- fix_slash.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace(/\/ aria-label="([^"]+)"/g, 'aria-label="$1" /');
-fs.writeFileSync('index.html', html);
-console.log('Fixed slash formatting.');
